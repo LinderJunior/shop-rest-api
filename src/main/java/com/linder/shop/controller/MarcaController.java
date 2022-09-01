99f005a491341f610335807bf3fe9baa1bda8c02
LinderJunior.com <linderju01@gmail.com>
2022-09-01 15:03:02 -0700

    crud marca

--- src/main/java/com/linder/shop/controller/MarcaController.java
+++ src/main/java/com/linder/shop/controller/MarcaController.java
@@ -1,12 +1,21 @@
 package com.linder.shop.controller;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.linder.shop.model.Marca;
+import com.linder.shop.repository.EstadoRepository;
 import com.linder.shop.service.MarcaService;
 
 @RestController
@@ -22,20 +31,26 @@ public class MarcaController {
 	public List<Marca>listar(){
 		return marcaService.listar();
 	}
+	//save
+	@PostMapping("/salvar")
+	public Marca salvar(@RequestBody Marca marca) {
+		return marcaService.salvar(marca);
+	}
 	
+	//update
+	@PutMapping("/actualizar")
+	public Marca actualizar(@RequestBody Marca marca) {
+		return marcaService.salvar(marca);
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//Delete
+	@DeleteMapping("/{id}")
+    public ResponseEntity<Object> excluir(@PathVariable("id") Long id){
+		Optional<Marca>marcaOptional = marcaService.findById(id);
+		if(!marcaOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Marca nao encontrada");
+		}
+    	marcaService.deletar(id);
+    	return ResponseEntity.status(HttpStatus.OK).body("Deletado com sucesso");
+    }
 }
--- src/main/java/com/linder/shop/service/MarcaService.java
+++ src/main/java/com/linder/shop/service/MarcaService.java
@@ -1,5 +1,7 @@
 package com.linder.shop.service;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,33 @@ public class MarcaService {
 		return marcaRepository.findAll();
 	}
 	
+	//save
+	public Marca salvar(Marca marca) {
+		marca.setData(new Date());
+		Marca novaMarca = marcaRepository.save(marca);
+		return novaMarca;
+	}
+	//update
+	public Marca update(Marca marca) {
+		marca.setData(new Date());
+		Marca novaMarca = marcaRepository.save(marca);
+		return novaMarca;
+	}
+	//delete
+	public void deletar(Long id) {
+		Marca marca = marcaRepository.findById(id).get();
+		marcaRepository.delete(marca);
+	}
+	
+	//getById
+	
+	public Optional<Marca> findById(Long id){
+		return marcaRepository.findById(id);
+	}
 	
 	
 	
 	
-
 	
 	
 	
@@ -37,5 +61,5 @@ public class MarcaService {
 	
 	
 	
-
+	
 }
